44e8c0e8daa11add1346c0777e75f970d8775632
KFR3-Value <kiliandlfrey@gmail.com>
2025-11-18 16:50:20 +0000

    modified:   trip_data.js

--- trip_data.js
+++ trip_data.js
@@ -8,7 +8,7 @@ window.tripDataMap = {
             "<strong>Group Size:</strong> 12 people"
         ],
         "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d99585.82815341797!2d-9.22772304865925!3d38.74368828603518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd19331a61e4f33b%3A0x400ebbde49036d0!2sLisbon%2C%20Portugal!5e0!3m2!1sen!2sch!4v1680000000000!5m2!1sen!2sch",
-        "Villas": [
+        "villas": [
             {
                 "name": "Apartment 1",
                 "description": "",
